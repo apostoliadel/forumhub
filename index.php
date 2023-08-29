8abd68afea9c6f9843e48be55c6110518e15ae41
Apostolia Delikosta <apostolia.delikosta@gmail.com>
2023-08-29 11:13:01 +0300

    Add new config and includes files
    * Add new config and includes files, fixed readability

---
+++ index.php
@@ -0,0 +1,109 @@
+<?php require "includes/header.php"; ?>
+
+    <!-- Main content container -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="main-col">
+            <div class="block">
+              <h1 class="pull-left">Welcome to Forum</h1>
+              <h4 class="pull-right">A Simple Forum</h4>
+              <div class="clearfix"></div>
+              <hr>
+              <ul id="topics">
+
+                <!-- Topic #1 -->
+                <li class="topic">
+                  <div class="row">
+                    <div class="col-md-2">
+                      <img class="avatar pull-left" src="img/gravatar.png" />
+                    </div>
+                    <div class="col-md-10">
+                      <div class="topic-content pull-right">
+                        <h3><a href="topic.html" class="topic-title">Empowering Your Inner Voice: Self-Love and Confidence</a></h3>
+                        <div class="topic-info">
+                          <a href="category.html">Personal Growth</a> >> <a href="profile.html">Apostolia Delikosta</a> >> Posted on: July 12, 2023
+                          <span class="color badge pull-right">3</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+
+                <!-- Topic #2 -->
+                <li class="topic">
+                  <div class="row">
+                    <div class="col-md-2">
+                      <img class="avatar pull-left" src="img/gravatar.png" />
+                    </div>
+                    <div class="col-md-10">
+                      <div class="topic-content pull-right">
+                        <h3><a href="topic.html" class="topic-title">Reflecting on Life Lessons and Milestones</a></h3>
+                        <div class="topic-info">
+                          <a href="category.html">Personal Growth</a> >> <a href="profile.html">Apostolia Delikosta</a> >> Posted on: July 12, 2023
+                          <span class="color badge pull-right">7</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+
+                <!-- Topic #3 -->
+                <li class="topic">
+                  <div class="row">
+                    <div class="col-md-2">
+                      <img class="avatar pull-left" src="img/gravatar.png" />
+                    </div>
+                    <div class="col-md-10">
+                      <div class="topic-content pull-right">
+                        <h3><a href="topic.html" class="topic-title">Small Acts of Kindness: Spreading Joy and Positivity</a></h3>
+                        <div class="topic-info">
+                          <a href="category.html">Everyday Joys</a> >> <a href="profile.html">Apostolia Delikosta</a> >> Posted on: July 12, 2023
+                          <span class="color badge pull-right">4</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+
+                <!-- Topic #4 -->
+                <li class="topic">
+                  <div class="row">
+                    <div class="col-md-2">
+                      <img class="avatar pull-left" src="img/gravatar.png" />
+                    </div>
+                    <div class="col-md-10">
+                      <div class="topic-content pull-right">
+                        <h3><a href="topic.html" class="topic-title">Yoga and Meditation: Sharing Your Journey and Insights</a></h3>
+                        <div class="topic-info">
+                          <a href="category.html">Mindfulness & Wellness</a> >> <a href="profile.html">Apostolia Delikosta</a> >> Posted on: July 12, 2023
+                          <span class="color badge pull-right">2</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+
+                <!-- Topic #5 -->
+                <li class="topic">
+                  <div class="row">
+                    <div class="col-md-2">
+                      <img class="avatar pull-left" src="img/gravatar.png" />
+                    </div>
+                    <div class="col-md-10">
+                      <div class="topic-content pull-right">
+                        <h3><a href="topic.html" class="topic-title">Time Management Strategies</a></h3>
+                        <div class="topic-info">
+                          <a href="category.html">Personal Growth</a> >> <a href="profile.html">Apostolia Delikosta</a> >> Posted on: July 12, 2023
+                          <span class="color badge pull-right">4</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </div>
+<?php require "includes/footer.php"; ?>
