d95c5c986a0f3733ac34857d7fc62c68260da64b
apostoliadel <apostolia.delikosta@gmail.com>
2023-08-29 15:28:33 +0300

    Updates

--- includes/footer.php
+++ includes/footer.php
@@ -1,39 +1,37 @@
+<!-- Sidebar -->
+<div class="col-md-4">
+  <div id="sidebar" class="sidebar">
 
-
-        <!-- Sidebar -->
-        <div class="col-md-4">
-          <div id="sidebar" class="sidebar">
-
-            <!-- List of categories -->
-            <div class="block">
-              <h3>Categories</h3>
-              <div class="list-group block">
-                <a href="#" class="list-group-item active">All Topics <span class="badge pull-right">14</span></a>
-                <a href="#" class="list-group-item">Mindfulness & Wellness<span class="color badge pull-right">4</span></a1>
-                <a href="#" class="list-group-item">Personal Growth<span class="color badge pull-right">9</span></a>
-                <a href="#" class="list-group-item">Nature & Environment<span class="color badge pull-right">12</span></a>
-                <a href="#" class="list-group-item">Everyday Joys<span class="color badge pull-right">7</span></a>
-                <a href="#" class="list-group-item">Creativity & Expression<span class="color badge pull-right">3</span></a>
-              </div>
-            </div>
-
-            <!-- Forum Statistics Block -->
-            <div class="block" style="margin-top: 20px">
-              <h3>Forum Statistics</h3>
-              <div class="list-group">
-                <a href="#" class="list-group-item">Total Number of Users:<span class="color badge pull-right">4</span></a>
-                <a href="#" class="list-group-item">Total Number of Topics:<span class="color badge pull-right">9</span></a>
-                <a href="#" class="list-group-item">Total Number of Categories:<span class="color badge pull-right">12</span></a>
-              </div>
-            </div>
-
-          </div>
-        </div>
+    <!-- List of categories -->
+    <div class="block">
+      <h3>Categories</h3>
+      <div class="list-group block">
+        <a href="#" class="list-group-item active">All Topics <span class="badge pull-right">14</span></a>
+        <a href="#" class="list-group-item">Mindfulness & Wellness<span class="color badge pull-right">4</span></a>
+        <a href="#" class="list-group-item">Personal Growth<span class="color badge pull-right">9</span></a>
+        <a href="#" class="list-group-item">Nature & Environment<span class="color badge pull-right">12</span></a>
+        <a href="#" class="list-group-item">Everyday Joys<span class="color badge pull-right">7</span></a>
+        <a href="#" class="list-group-item">Creativity & Expression<span class="color badge pull-right">3</span></a>
       </div>
-    </div><!-- /.container -->
+    </div>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="<?php echo APPURL; ?>/js/bootstrap.js"></script>
-  </body>
+    <!-- Forum Statistics Block -->
+    <div class="block" style="margin-top: 20px">
+      <h3>Forum Statistics</h3>
+      <div class="list-group">
+        <a href="#" class="list-group-item">Total Number of Users:<span class="color badge pull-right">4</span></a>
+        <a href="#" class="list-group-item">Total Number of Topics:<span class="color badge pull-right">9</span></a>
+        <a href="#" class="list-group-item">Total Number of Categories:<span class="color badge pull-right">12</span></a>
+      </div>
+    </div>
+
+  </div>
+</div>
+</div>
+</div><!-- /.container -->
+
+<!-- Bootstrap core JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="<?php echo APPURL; ?>/js/bootstrap.js"></script>
+</body>
 </html>
